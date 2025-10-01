e7252109ffd9a90cb5ad8de14bd6c5ac35be72ec
wanwan <dailysleepy@qq.com>
2025-10-01 19:05:01 +0800

    新增_any

--- _any.h
+++ _any.h
@@ -1,11 +1,41 @@
 #pragma once
 #include <iostream>
+#include <any>
 using namespace std;
 
 namespace _any
 {
 	inline void main()
 	{
-		cout << "hello world" << endl;
+		any a;
+		cout << a.type().name() << endl; // 动态类型 void, 可以理解为更安全智能的 void*
+		cout << typeid(a).name() << endl; // 静态类型 class std::any
+
+		a = "abc";
+		a.reset();
+		if (!a.has_value()) cout << "a has no value" << endl;
+
+		a = make_any<string>("abc");
+		a.emplace<string>("abc"); // make_any 和 emplace 原地构造
+
+		// 使用 try 访问
+		try
+		{
+			cout << any_cast<string>(a) << endl; // abc
+			cout << any_cast<int>(a) << endl; // bad any cast
+		}
+		catch (const bad_any_cast& e)
+		{
+			cout << e.what() << endl;
+		}
+
+		// 使用指针访问
+		auto p = any_cast<int>(&a); // 使用 any_cast<T>(any*) 的重载
+		if (p) cout << *p << endl;
+		else cout << "nullptr" << endl; // nullptr
+
+		// 使用 typeid 先判断再访问
+		if (a.type() == typeid(string))
+			cout << any_cast<string>(a) << endl; // abc
 	}
 }
